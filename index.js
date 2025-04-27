5beb074974477a954a28d55ae5f8bcaeb1b33d6a
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-27 10:16:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config(); // This will load the environment variables from .env file
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const port = process.env.PORT || 3000;
