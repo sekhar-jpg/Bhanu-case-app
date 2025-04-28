62646e5777aa8ad2f7471dceac505fb4b9d3877b
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-28 08:32:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const caseRoutes = require('./routes/caseRoutes');
+require('dotenv').config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/cases', caseRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Server is running...');
+    res.send('API Running...');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
