cba033afbc93e3d1f5555e1efff996be40f4f593
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-28 11:12:54 +0530

    Update caseModel.js

--- models/caseModel.js
+++ models/caseModel.js
@@ -1,44 +1,27 @@
-// caseModel.js
 const mongoose = require('mongoose');
 
-// Define the schema for the case model
 const caseSchema = new mongoose.Schema({
   patientName: {
     type: String,
     required: true
   },
-  age: {
-    type: Number,
-    required: true
-  },
-  gender: {
+  phoneNumber: {
     type: String,
-    enum: ['Male', 'Female', 'Other'],
     required: true
   },
-  symptoms: {
-    type: [String],
+  caseDetails: {
+    type: String,
     required: true
-  },
-  diagnosis: {
-    type: String
-  },
-  remedy: {
-    type: String
   },
   followUpDate: {
     type: Date
   },
-  contactNumber: {
-    type: String
-  },
-  caseCreatedAt: {
+  dateSubmitted: {
     type: Date,
     default: Date.now
   }
 });
 
-// Create the model using the schema
 const Case = mongoose.model('Case', caseSchema);
 
 module.exports = Case;
