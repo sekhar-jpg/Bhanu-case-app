1074219f2bd00db1d5ea636cf206b3f9a1dc5659
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-28 08:22:45 +0530

    Update Case.js

--- models/Case.js
+++ models/Case.js
@@ -1,55 +1,45 @@
 const mongoose = require('mongoose');
 
 const caseSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  gender: String,
-  maritalStatus: String,
-  occupation: String,
-  address: String,
-  phone: String,
-  dateOfVisit: Date,
-  chiefComplaints: [
-    {
-      complaint: String,
-      duration: String,
-      description: String
-    }
-  ],
-  historyOfPresentIllness: String,
-  pastHistory: {
-    childhoodDiseases: String,
-    surgeries: String,
-    majorIllnesses: String
-  },
-  familyHistory: String,
-  personalHistory: {
-    appetite: String,
-    cravingsAversions: String,
-    thirst: String,
-    bowelMovement: String,
-    urine: String,
-    sleep: String,
-    dreams: String,
-    sweat: String,
-    thermalNature: String,
-    habits: String,
-    menstrualHistory: String
-  },
-  mentalSymptoms: String,
-  generalRemarks: String,
-  doctorObservations: String,
-  prescription: [
-    {
-      date: Date,
-      remedyName: String,
-      potency: String,
-      dose: String,
-      instructions: String
-    }
-  ],
-  followUpDate: Date,
+    name: String,
+    age: Number,
+    gender: String,
+    maritalStatus: String,
+    occupation: String,
+    address: String,
+    phone: String,
+    dateOfVisit: Date,
+    complaints: [{ complaint: String, duration: String, description: String }],
+    historyOfPresentIllness: String,
+    pastHistory: {
+        childhoodDiseases: String,
+        surgeriesInjuries: String,
+        majorIllnesses: String,
+    },
+    familyHistory: String,
+    personalHistory: {
+        appetite: String,
+        cravingsAversions: String,
+        thirst: String,
+        bowelMovement: String,
+        urine: String,
+        sleep: String,
+        dreams: String,
+        sweat: String,
+        thermal: String,
+        habits: String,
+        menstrualHistory: String,
+    },
+    mentalSymptoms: String,
+    generalRemarks: String,
+    doctorObservations: String,
+    prescriptions: [{
+        date: Date,
+        remedyName: String,
+        potency: String,
+        dose: String,
+        instructions: String,
+    }]
 });
 
-const Case = mongoose.model('Case', caseSchema);
-module.exports = Case;
+module.exports = mongoose.model('Case', caseSchema);
