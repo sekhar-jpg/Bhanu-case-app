7be966999f473fe2a05b1e4c600155f37d70cff0
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-28 17:06:13 +0530

    Update caseRoutes.js

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -5,7 +5,10 @@ const caseController = require('../controllers/caseController');
 // Handle case submission
 router.post('/', caseController.createCase);
 
-// Retrieve all cases (for follow-ups)
+// Retrieve all cases (for admin view)
 router.get('/', caseController.getCases);
+
+// Retrieve today's follow-up list
+router.get('/followups/today', caseController.getTodayFollowUps);
 
 module.exports = router;
