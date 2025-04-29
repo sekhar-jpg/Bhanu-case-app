a3e2d5bcf28c00bfc5b6e29c209597b736bfd406
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-04-29 15:49:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,7 @@ app.post('/api/get-remedy', (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
